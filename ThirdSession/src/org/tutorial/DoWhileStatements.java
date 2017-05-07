19096f927f876f70ce0c8fc5fa0aa280f8537aaa
mnandan06 <mnandan06@gmail.com>
2017-05-07 13:55:38 +0530

    exercise ninth programs control statements

---
+++ ThirdSession/src/org/tutorial/DoWhileStatements.java
@@ -0,0 +1,15 @@
+package org.tutorial;
+
+public class DoWhileStatements 
+{
+	public static void main(String[] args) 
+	{
+		int a = 7;
+		
+		do {
+				System.out.println(" number : " + a);
+				a--;
+		   }while(a > 0);
+		
+	}
+}
---
+++ ThirdSession/src/org/tutorial/WhileStatements.java
@@ -0,0 +1,16 @@
+package org.tutorial;
+
+public class WhileStatements 
+{
+	public static void main(String[] args) 
+	{
+		// while example
+		
+		int count = 1;
+		while(count < 10)
+		{
+			System.out.println(" number count " + count);
+			count++;
+		}
+	}
+}
