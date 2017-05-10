d74055d7576bb6dd14ecb7a2255d17adddc53266
mnandan06 <mnandan06@gmail.com>
2017-05-10 17:32:25 +0530

    "constructor" ninth chapter exercises

---
+++ NinthChapter/src/org/ninthchapter/java/Marks.java
@@ -0,0 +1,22 @@
+package org.ninthchapter.java;
+
+
+
+public class Marks {
+	// constructor in java
+	
+	double rollno;
+	double subject1;
+	double subject2;
+	double subject3;
+	
+	Marks(double r, double s1, double s2, double s3) {
+		this.rollno = r;
+		this.subject1 = s1;
+		this.subject2 = s2;
+		this.subject3 = s3;
+		
+	}
+
+}
+
