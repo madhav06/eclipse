1b77b4337ec74459c97535f0a2046a7ec5b4aa2e
mnandan06 <mnandan06@gmail.com>
2017-05-10 16:59:51 +0530

    Construcor in java " fourth session"

---
+++ FourthSession/src/org/tutorial/Algebra.java
@@ -0,0 +1,22 @@
+package org.tutorial;
+
+public class Algebra {
+// constructor in java
+	double length;
+	double breadth;
+	
+	Algebra() {
+		length = 20;
+		breadth = 10;
+		
+		// parameterized constructor in java
+		
+		/*
+		 * Algebra(int l, int b) {
+		 * this.length = l;
+		 * this.breadth = b;
+		 * 
+		 * }
+		 */
+	}
+}
---
+++ FourthSession/src/org/tutorial/AlgebraTest.java
@@ -0,0 +1,20 @@
+package org.tutorial;
+
+public class AlgebraTest {
+	public static void main(String[] args) {
+		// In case of constructor
+		Algebra a = new Algebra();
+		System.out.println(" length of rectangle: " + a.length);
+		System.out.println(" breadth of rectangle: " + a.breadth);
+		
+		// In case of parameterized constructor 
+		
+		/*
+		 *Algebra a = new Algebra(30, 20);
+		 *System.out.println(" length of rectangle: " + a.length);
+		 *System.out.println(" breadth of rectangle: " + a.breadth); 
+		 * 
+		 */
+	}
+
+}
