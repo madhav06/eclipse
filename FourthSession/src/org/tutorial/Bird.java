fa11f0b17a388e6294767e80c4cc2e7c999126ef
mnandan06 <mnandan06@gmail.com>
2017-05-10 17:43:56 +0530

    " fourth session "

---
+++ FourthSession/src/org/tutorial/Bird.java
@@ -0,0 +1,11 @@
+package org.tutorial;
+
+public class Bird {// Superclass
+	
+ public void eat() {
+	 System.out.println(" All bird eat. ");
+	 System.out.println(" ");
+	 System.out.println(" Bird love to fly in open space. ");
+	 
+ }
+}
---
+++ FourthSession/src/org/tutorial/Sparrow.java
@@ -0,0 +1,11 @@
+package org.tutorial;
+
+public class Sparrow extends Bird{ // Inheritance 
+	
+	public static void main(String[] args) {
+		
+		Sparrow spw = new Sparrow();
+		spw.eat();
+		
+	}
+}
