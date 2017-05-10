41319b7df9bcb1550ef8a43b988f5480ce8d7e80
mnandan06 <mnandan06@gmail.com>
2017-05-10 16:40:34 +0530

    fourth session java programs

---
+++ FourthSession/src/org/tutorial/RecDemo.java
@@ -0,0 +1,13 @@
+package org.tutorial;
+
+public class RecDemo {
+	public static void main(String[] args) {
+		
+		thisKeyword th = new thisKeyword();
+		
+		th.setDimensions(10, 20);
+		
+		System.out.println(" Rectangle dimensions: " + "breadth: " + th.breadth + " AND "+ "length: " +th.length);
+	}
+
+}
---
+++ FourthSession/src/org/tutorial/thisKeyword.java
@@ -0,0 +1,13 @@
+package org.tutorial;
+
+public class thisKeyword 
+{
+	int length, breadth;
+	
+	public void setDimensions(int l, int b){
+		// this keyword is used to refer to the current objects
+		
+		this.length = l;
+		this.breadth = b;
+	}
+}
