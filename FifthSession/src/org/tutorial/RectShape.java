f0c1a7fba4876b8f72e4f69b5ef35cd477f8ffe5
mnandan06 <mnandan06@gmail.com>
2017-05-11 15:27:04 +0530

    " fifth session Strings and methods"

---
+++ FifthSession/src/org/tutorial/RectShape.java
@@ -0,0 +1,12 @@
+package org.tutorial;
+
+public class RectShape {
+	int length, breadth;
+	public void setLength(int l) {
+		length = l;
+	}
+	
+	int getLength() {
+		return length;
+	}
+}
---
+++ FifthSession/src/org/tutorial/RectShapeTest.java
@@ -0,0 +1,14 @@
+package org.tutorial;
+
+public class RectShapeTest {
+	
+	public static void main(String[] args) {
+		
+		RectShape r = new RectShape();
+		r.setLength(20);
+		
+		int l = r.getLength();
+		System.out.println(" Length of rectngle: " + l);
+	}
+
+}
