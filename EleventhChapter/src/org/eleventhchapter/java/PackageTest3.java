a152f06be45cb7d4b12d8bda7db37ac689333636
mnandan06 <mnandan06@gmail.com>
2017-05-11 19:08:43 +0530

    " exercise eleventh programs"

---
+++ EleventhChapter/src/org/eleventhchapter/java/PackageTest3.java
@@ -0,0 +1,25 @@
+
+package org.eleventhchapter.java;
+
+import package2.*;
+
+class ClassC extends ClassB {
+	
+	int n = 20;
+	void displayC() {
+		System.out.println(" Class C ");
+		System.out.println(" m = " + m);
+		System.out.println(" n = " + n);
+	}
+
+}
+
+class PackageTest3 {
+	public static void main(String[] args) {
+		
+		ClassC objectC = new ClassC();
+		
+		objectC.displayB();
+		objectC.displayC();
+	}
+}
---
+++ EleventhChapter/src/package1/ClassA.java
@@ -0,0 +1,9 @@
+//package org.eleventhchapter.java;
+
+package package1;
+public class ClassA {
+	public void displayA() {
+		System.out.println(" Class A ");
+	}
+
+}
